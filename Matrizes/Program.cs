48ff9624a25632aaa507a7701b8c6d5687c0ce9c
Sergio Luis Francisco Pinheiro <spinheiro.lf@gmail.com>
2023-03-28 10:17:05 -0300

    Cria funcao de contar pares e impares

--- Matrizes/Program.cs
+++ Matrizes/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Matrizes;
+
+namespace evenodd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            countEvenOdd countEvenOdd = new countEvenOdd();
+
+            int[,] array = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 9 } };
+
+            int result = countEvenOdd.CountEvenOdd(array);
+
+           Console.WriteLine(result);
+        }
+    }
+}
---
+++ Matrizes/countEvenOdd.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrizes
+{
+    public class countEvenOdd
+    {
+        public int CountEvenOdd(int[,] matrix)
+        {
+            int evenCount = 0;
+            int oddCount = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] % 2 == 0)
+                    {
+                        evenCount++;
+                    }
+                    else
+                    {
+                        oddCount++;
+                    }
+                }
+            }
+
+            return evenCount > oddCount ? evenCount : oddCount;
+        }
+    }
+}
